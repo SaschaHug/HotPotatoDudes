e324ca3f699d12b9a5e51f435bce3ebfd7e85cb5
IsThisTheFuture <mail@tobiaslamm.de>
2017-07-30 17:37:15 +0200

    added passwordRecovery

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,11 +1,54 @@
 //var login = require('../src/data/user.json');
-//var fs = require('fs');
+var fs = require('fs');
 const jwt = require('jsonwebtoken');
+let User = require('../src/data/user.json');
+
 
 exports.login = function(req, res) {
-  let User = require('../src/data/user.json');
   authenticate(User, res, req);
 };
+
+
+//exports.passwordRecovery = function(req, res) {
+  function passwordRecovery(req, res){
+  if (!res.locals.authenticated) {
+    return res.status(401).send({
+      //message: res.message
+      message: 'not authenticated'
+    });
+  }
+
+    var User = require('../src/data/user.json');
+    if (User.password === req.body.password) {
+      User.password = req.body.newPassword;
+      let token = jwt.sign(User, 'MegaUltraHiddenSuperSecret');
+
+      fs.writeFile('./src/data/user.json', JSON.stringify(User), 'utf-8', (err) => {
+        if (err) {
+          res.status(500).json({error: err});
+        } else {
+          res.status(200).send(
+            {
+              'status': 'password was successfully changed',
+              'token': token
+            });
+
+
+
+      //res.status(200).json({'token': token});
+    }
+  });
+
+
+}
+else {
+  res.status(401).send({
+    message: 'wrong password, please try again'
+  });
+
+
+}
+}
 
 
 function authenticate(User, res, req){
@@ -15,4 +58,26 @@ function authenticate(User, res, req){
    } else {
      res.status(401).json({'token': ''});
    }
+}
+
+
+exports.checkIfAuthorised = function (req, res) {
+  let encodedToken = req.body.token;
+  let decodedToken = verify(encodedToken);
+  if (decodedToken.password === User.password) {
+    res.locals.authenticated = true;
+    passwordRecovery(req, res);
+  } else {
+    res.status(401).send('Not Authorized!');
+  }
+}
+
+function verify(token) {
+  var decoded = false;
+  try {
+    decoded = jwt.verify(token, 'MegaUltraHiddenSuperSecret');
+  } catch (e) {
+    decoded = false; // still false
+  }
+  return decoded;
 }
